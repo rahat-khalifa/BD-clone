c118385af511b9092acaffc2ee4c8cc48b9bb5ba
rahat-khalifa <78908978+rahat-khalifa@users.noreply.github.com>
2021-02-11 23:46:14 +0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install wget php curl
+pkg install git
+pip2 install requests
+pip2 install mechanize
+pip2 install bs4
+clear
+echo "\033[1;32mNOW YOU ARE READY TO RUN THIS TOOL\033[1;0m"
+echo "\033[1;36m-------------------------------"
+echo "\033[1;33m|   TYPE python2 BD.py         |"
+echo "\033[1;36m-------------------------------"
